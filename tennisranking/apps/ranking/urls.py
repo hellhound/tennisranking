f330bda03b9411ff5b9ead1cbc258ff0537d6248
Jean-Pierre Chauvel <jchauvel@gmail.com>
2013-09-01 21:23:00 -0500

    fix #2 ranking/home screen mock up and a placeholder template for duels

--- tennisranking/apps/ranking/urls.py
+++ tennisranking/apps/ranking/urls.py
@@ -1,7 +1,12 @@
 # -*- coding:utf-8 -*-
 from django.conf.urls import patterns, url
-from django.views.generic.base import TemplateView
+from django.views.generic.base import RedirectView, TemplateView
 
 urlpatterns = patterns('',
-    url(r'^$', TemplateView.as_view(template_name='ranking/dashboard.html')),
+    url(r'^$', RedirectView.as_view(url='/ranking/'),
+    name='home'),
+    url(r'^ranking/$', TemplateView.as_view(template_name='ranking/ranking.html'),
+    name='ranking'),
+    url(r'^duels/$', TemplateView.as_view(template_name='ranking/duels.html'),
+    name='duels'),
 )
